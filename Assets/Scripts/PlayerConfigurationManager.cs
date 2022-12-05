4c78890e207b8dfcd2dc9ab297850bafb889a645
Galen O <30817530+GalenODonnell@users.noreply.github.com>
2022-12-05 18:58:49 -0500

    updated bike area walls

---
+++ Assets/Scripts/PlayerConfigurationManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerConfigurationManager : MonoBehaviour
+{
+    public PlayerInput input;
+    public int playerIndex;
+    public bool isReady;
+    public Material playerMaterial;
+}
